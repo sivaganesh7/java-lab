ffc0a1215651d1181d8b87ddbbff7d91e4cd60ef
Siva Ganesh Pepakayala <157547445+sivaganesh7@users.noreply.github.com>
2024-10-19 07:48:30 +0530

    Update InterfaceExample.java

--- week 05/InterfaceExample.java
+++ week 05/InterfaceExample.java
@@ -1,46 +1,47 @@
-// interface
-interface Animal {
-    void sound(); 
-    void eat();  
+interface shape{
+    void area();
+} 
+interface coloring{
+    void colour(String c);
 }
-
-
-class Dog implements Animal {
-    @Override
-    public void sound() {
-        System.out.println("Dog barks");
+class rectangle implements shape,coloring{
+    double l,b;
+    rectangle(double l,double b){
+        this.l=l;
+        this.b=b;
     }
-
-    @Override
-    public void eat() {
-        System.out.println("Dog eats dog food");
+    
+    public void area(){
+        System.out.println("area of rectangle: "+(l*b));
     }
-}
-
-
-class Cat implements Animal {
-    @Override
-    public void sound() {
-        System.out.println("Cat meows");
-    }
-
-    @Override
-    public void eat() {
-        System.out.println("Cat eats cat food");
+    public void colour(String c){
+        String col=c;
+        System.out.println("rectangle color is :"+col);
     }
 }
-
-
-public class InterfaceExample {
+class circle implements shape,coloring{
+    int r;
+    circle(int r){
+        this.r=r;
+        //this.b=b;
+    }
+    public void area(){
+        System.out.println("area of circle: "+(3.14*r*r));
+    }
+   public void colour(String c){
+        String col=c;
+        System.out.println("rectangle color is :"+col);
+   }
+}
+public class interfaces{
     public static void main(String[] args) {
-        Animal myDog = new Dog();
-        Animal myCat = new Cat();
-
-        myDog.sound(); // Output: Dog barks
-        myDog.eat();   // Output: Dog eats dog food
-
-        myCat.sound(); // Output: Cat meows
-        myCat.eat();   // Output: Cat eats cat food
+        rectangle r=new rectangle(10, 5);
+        circle c=new circle(2);
+        
+        r.area();
+        r.colour("red");
+     
+        c.area();
+        c.colour("green");
     }
 }
-
